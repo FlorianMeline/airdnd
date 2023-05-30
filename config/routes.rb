31ddf0f75cef82729d87b9307be16e05f52f0dcb
Florian Meline <florian_meline@live.fr>
2023-05-30 11:50:00 +0200

    correction image logo ext

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  resource :profile, only: [:show]
+
+  resources :teams, only: [:index, :show] do
+    resources :contracts, only: [:create]
+  end
+  resources :contracts, only: [:index]
+
+  namespace :owner do
+    resources :teams, only: [:index, :new, :create]
+    resources :contracts, only: [:update, :index]
+  end
 end
