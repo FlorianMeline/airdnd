94d9908bcc70246b47c6f240a8b1185321ddb51d
Florian Meline <florian_meline@live.fr>
2023-05-31 16:05:15 +0200

    last push de la journée

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,12 @@
 class TeamsController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: :index
   def index
     @teams = Team.all
   end
-  
+
   def show
     @team = Team.find(params[:id])
     @contract = Contract.new
   end
- 
+
 end
