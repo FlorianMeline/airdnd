e350d2a40e285871c85efe63ba9de473bc14865c
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-06-01 10:14:32 +0200

    profil page

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,10 +1,2 @@
 class ContractsController < ApplicationController
-  # def create
-  #   @item = Item.new(name: item_params[:name])
-  #   if @item.save
-  #     redirect_to ...
-  #   else
-  #     render:new
-  #   end
-  # end
 end
