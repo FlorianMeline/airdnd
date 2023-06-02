6991e017f20a2640c058384709f185804432a7ec
Ulysse <ulysse@ik.me>
2023-06-02 12:54:37 +0200

    fonction accepter et refuser contrat

--- app/controllers/owner/contracts_controller.rb
+++ app/controllers/owner/contracts_controller.rb
@@ -4,6 +4,22 @@ class Owner::ContractsController < ApplicationController
   end
 
   def update
-    
+    @contract = Contract.find(params[:id])
+    @contract.update(user_contracts_params)
+
+    if @contract.status == "accepted"
+      flash[:success] = "Contract accepté"
+      redirect_to owner_contracts_path
+    else
+
+      flash[:danger] = "Contract refusé"
+      redirect_to owner_contracts_path
+    end
+  end
+
+  private
+
+  def user_contracts_params
+    params.require(:contract).permit(:status, :id)
   end
 end
--- app/controllers/owner/teams_controller.rb
+++ app/controllers/owner/teams_controller.rb
@@ -4,20 +4,20 @@ module Owner
       @user_teams = current_user.teams
     end
 
-  def new
-    @team = Team.new
-  end
+    def new
+      @team = Team.new
+    end
 
-  def create
-    team = Team.new(team_params)
-    team.owner = current_user
-    team.save
-    redirect_to teams_path
+    def create
+      team = Team.new(team_params)
+      team.owner = current_user
+      team.save
+      redirect_to teams_path
 
-  end
+    end
 
-  def team_params
-    params.require(:team).permit(:name, :description, :price_per_day, :dungeon_type)
+    def team_params
+      params.require(:team).permit(:name, :description, :price_per_day, :dungeon_type)
+    end
   end
 end
-
