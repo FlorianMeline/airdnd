d9267c6d90865c45784b0439d81b0f08350bc633
Florian Meline <florian_meline@live.fr>
2023-06-02 14:28:23 +0200

    correction des fonctionnalités

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,7 +5,7 @@ class Team < ApplicationRecord
   include PgSearch::Model
 
   pg_search_scope :search_by_dungeon_type_and_success_rate,
-                against: [ :dungeon_type, :success_rate ],
+                against: [ :dungeon_type, :success_rate, :name, :price_per_day ],
                 using: {
                 tsearch: { prefix: true } # <-- now `superman batm` will return something!
                 }
